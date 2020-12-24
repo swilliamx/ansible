5f1cb8c277bf2d52962a1dba530002b5b597759c
swilliamx <solomnwilliams@gmail.com>
2020-12-24 12:32:08 -0500

    Update ec2-list.sh

--- ec2-list.sh
+++ ec2-list.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-aws ec2 describe-instances --query 'Reservations[*].Instances[*].{InstanceName:Tags[?Key==`Name`] | [0].Value,PublicIPAddress:PublicIpAddress,PublicDnsName:PublicDnsName,InstanceID:InstanceId,InstanceType:InstanceType,InstanceState:State.Name,SubnetID:SubnetId,VPCID:VpcId}' --output table --region us-east-1
+aws ec2 describe-instances --query 'Reservations[*].Instances[*].{InstanceName:Tags[?Key==`Name`] | [0].Value,PublicIPAddress:PublicIpAddress,PublicDnsName:PublicDnsName,InstanceID:InstanceId,InstanceType:InstanceType,InstnceState:State.Name,SubnetID:SubnetId,VPCID:VpcId}' --output table --region us-east-1
